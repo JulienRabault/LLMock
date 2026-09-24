34924ebce1fdb84abd481c0445e5c1d4ffaf4d66
julien <julienrabault@icloud.com>
2026-09-24 19:16:39 +0200

    chore: release 0.2.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,7 +2,7 @@
 
 All notable changes to this project should be documented in this file.
 
-## [Unreleased]
+## [0.2.2] - 2026-09-24
 
 ### Fixed
 
--- llmock/__init__.py
+++ llmock/__init__.py
@@ -4,6 +4,6 @@ A local server that speaks the HTTP of ten LLM providers, breaks on demand,
 and judges how your client coped. See https://github.com/JulienRabault/LLMock.
 """
 
-__version__ = "0.2.1"
+__version__ = "0.2.2"
 
 __all__ = ["__version__"]
